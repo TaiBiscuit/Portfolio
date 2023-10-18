748225f42e818c09faf5c202c352e2365685c2b2
nicolc <nicolas.l.campos1@gmail.com>
2023-10-18 15:09:23 -0300

    extras

--- src/components/Extras/Extras.jsx
+++ src/components/Extras/Extras.jsx
@@ -1,18 +1,28 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import '../../styles/extras.css'
 import { ViewModeContext } from "../../context/ViewModeContext";
-import { LanguageContext } from "../../context/LanguageContext";
 
 export const Extras = () => {
     const {lightMode} = useContext(ViewModeContext);
-    const [loading, setLoading] = useState(true);
-    const {language} = useContext(LanguageContext);
 
     return(
         <>
         <h1 className="header-h1" id="projects-header">Extras</h1>
         <div className="extras-zone">
-
+            {
+                lightMode ? 
+                <>
+                <a href="https://www.frontendmentor.io/profile/TaiBiscuit" target="_blank"><button className="extra-btn-B">Frontend Mentor</button></a>
+                <a href="https://leetcode.com/TaiBiscuit/" target="_blank"><button className="extra-btn-B">LeetCode</button></a>
+                <a href="https://www.codewars.com/users/TaiBiscuit" target="_blank"><button className="extra-btn-B">CodeWars</button></a>
+                </>
+                :
+                <>
+                <a href="https://www.frontendmentor.io/profile/TaiBiscuit" target="_blank"><button className="extra-btn-A">Frontend Mentor</button></a>
+                <a href="https://leetcode.com/TaiBiscuit/" target="_blank"><button className="extra-btn-A">LeetCode</button></a>
+                <a href="https://www.codewars.com/users/TaiBiscuit" target="_blank"><button className="extra-btn-A">CodeWars</button></a>
+                </>
+            }
         </div>
         </>
     )
