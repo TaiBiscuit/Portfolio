2d425633b6f17287d6c66b77a7e0677eff9b119a
nicolc <nicolas.l.campos1@gmail.com>
2024-02-20 21:53:31 -0300

    Fix

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -95,11 +95,11 @@ export const About = () => {
             <h1 className="header-h1">About me</h1>
             <div className="about-me-zone">
                 <div className="about-me-txt">
-                    <p>Hello! My name is Nico, a Frontend developer from Argentina. I am a Computer Science student at UBA (Unviersidad de Buenos Aires) and learning Fullstack development at Coderhouse and I am interested in everything referred to web development. I can also speak Spanish at a native level, and Japanese at an intermediate level. I am currently looking for my first work experience in the IT field, in the meantime I keep learning and making projects. Click on my dog to look at my contact information!
+                    <p>Hello! My name is Nico, a Frontend developer from Argentina. I am a Computer Science student at UBA (Universidad de Buenos Aires) and learning Fullstack development on my own. I can speak Spanish at a native level, and Japanese at an intermediate level. I consider myself highly adaptable, motivated, responsible and well organized person who is always open to learn new technologies.
                     </p>
                 </div>
                 <div className="about-me-picture">
-                    <a href="#contact"><img src="https://avatars.githubusercontent.com/u/113484831?v=4" alt="" className="about-me-img"/></a> 
+                    <a href="#contact"><img src="https://i.ibb.co/FmDrdbZ/1kjh-1.png" alt="" className="about-me-img"/></a> 
                 </div>
             </div>
             <h1 className="header-h1">Technologies that I use</h1>
@@ -126,12 +126,12 @@ export const About = () => {
             <h1 className="header-h1">Sobre mí</h1>
             <div className="about-me-zone">
             <div className="about-me-txt">
-                <p>¡Hola! Soy Nico, desarrollador Frontend. Soy estudiante de la carrera "Licenciatura en Ciencias de la Computación" de la UBA y estoy haciendo el curso de Fullstack en Coderhouse, y estoy interesado en todo lo referente al desarrollo web. Tengo un nivel de inglés bilingüe e intermedio de japonés.Actualmente me encuentro en la búsqueda de mi primera experiencia laboral en el campo de IT, mientras tanto sigo aprendiendo y realizando proyectos. ¡Haga click en mi perro para contactarme!
+                <p>¡Hola! Soy Nico, desarrollador Frontend. Soy estudiante de la carrera Licenciatura en Ciencias de la Computación de la UBA y estudio desarrollo web por mi cuenta. Tengo un nivel de inglés bilingüe e intermedio de japonés. Me considero una persona adaptable, motivada, responsable y bien organizada que siempre está abierta a aparender nuevas tecnologías.
                 </p>
                 
             </div>
             <div className="about-me-picture">
-                <a href="#contact"><img src="https://avatars.githubusercontent.com/u/113484831?v=4" alt="" className="about-me-img"/></a>
+                <a href="#contact"><img src="https://i.ibb.co/FmDrdbZ/1kjh-1.png" alt="" className="about-me-img"/></a>
             </div>
             </div>
             <h1 className="header-h1">Tecnologias que uso</h1>
